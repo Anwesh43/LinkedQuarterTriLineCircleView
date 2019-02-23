3802abc1c376cdb7ef38758a0d9ad267a452e39a
Anwesh43 <anweshthecool0@gmail.com>
2019-02-23 17:58:42 +0530

    adding QTLCNode class

--- quartertrilinecircleview/src/main/java/com/anwesh/uiprojects/quartertrilinecircleview/QuarterTriLineCircleView.kt
+++ quartertrilinecircleview/src/main/java/com/anwesh/uiprojects/quartertrilinecircleview/QuarterTriLineCircleView.kt
@@ -120,4 +120,48 @@ class QuarterTriLineCircleView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class QTLCNode(var i : Int, val state : State = State()) {
+
+        private var next : QTLCNode? = null
+        private var prev : QTLCNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = QTLCNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawQTLCNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : QTLCNode {
+            var curr : QTLCNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this 
+        }
+    }
 }

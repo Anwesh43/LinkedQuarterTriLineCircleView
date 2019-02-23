35d92f48335eb32c34d862eb1ad7128cab5de4eb
Anwesh43 <anweshthecool0@gmail.com>
2019-02-23 19:11:52 +0530

    adding delay as constant

--- quartertrilinecircleview/src/main/java/com/anwesh/uiprojects/quartertrilinecircleview/QuarterTriLineCircleView.kt
+++ quartertrilinecircleview/src/main/java/com/anwesh/uiprojects/quartertrilinecircleview/QuarterTriLineCircleView.kt
@@ -21,6 +21,7 @@ val sizeFactor : Float = 2.9f
 val strokeFactor : Int = 90
 val foreColor : Int = Color.parseColor("#4CAF50")
 val backColor : Int = Color.parseColor("#212121")
+val delay : Long = 20
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse()).toFloat()
@@ -101,7 +102,7 @@ class QuarterTriLineCircleView(ctx : Context) : View(ctx) {
             if (animated) {
                 cb()
                 try {
-                    Thread.sleep(50)
+                    Thread.sleep(delay)
                     view.invalidate()
                 } catch(ex : Exception) {
 
